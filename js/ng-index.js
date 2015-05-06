d9dfb85693e174b48c36d1d7fe1a02c1b29b06f1
nicoson <nicoson@163.com>
2015-05-06 16:34:32 -0400

    version 1.0.0, with angular directive templateUrl bug unsolved

--- js/ng-index.js
+++ js/ng-index.js
@@ -7,9 +7,8 @@ angular.module("indexModule", ["ngRoute"])
 }])
 
 .controller("contentCtrl", ["$scope","$routeParams","$http", function($scope, $routeParams, $http){
-	$http.get("resource/"+$routeParams.url+".json").success(function(data){
-		$scope.data = data;
-	});
+	$scope.url = $routeParams.url;
+	// $scope.url = "ubuntu";
 }])
 
 .config(["$routeProvider", function($routeProvider){
@@ -27,6 +26,48 @@ angular.module("indexModule", ["ngRoute"])
 	});
 }])
 
+.directive("navUnit", function(){
+	return{
+		restrict: "AEC",
+		scope: {term: "=term"},
+		link: function(scope, ele, attr){
+			//ele.css("background-color",scope.term.color);
+			ele.on("click", function(){
+				location.replace('#/content/'+scope.term.url);
+			});
+			ele.on("mouseenter", function(){
+				ele.css("background",scope.term.color);
+			});
+			ele.on("mouseleave", function(){
+				ele.css("background","#6f5499");
+			});
+		}
+	}
+})
+
+.directive("navbar", function(){
+	return{
+		restrict: "AEC",
+		link: function(scope, ele, attr){
+			var navbarhight = ele.offset().top;
+			var status = 0;
+			$(window).on("scroll", function(){
+				if($(this).scrollTop() >= navbarhight && status==0){
+					ele.toggleClass("nav-fix");
+					status = 1;
+					$("section").css("padding","116px 0 50px 0");
+				}else if($(this).scrollTop() < navbarhight && status==1){
+					ele.toggleClass("nav-fix");
+					status = 0;
+					$("section").css("padding","50px 0 50px 0");
+				}
+				//console.log(scope.navCtr);
+				scope.$apply();
+			});
+		}
+	}
+})
+
 .directive("hpUnit", function(){
 	return{
 		restrict: "AEC",
@@ -34,8 +75,48 @@ angular.module("indexModule", ["ngRoute"])
 		link: function(scope, ele, attr){
 			ele.css("background-color",scope.term.color);
 			ele.on("click", function(){
-				location.href = '#/content/'+scope.term.url;
+				//location.href = '#/content/'+scope.term.url;
+				location.replace('#/content/'+scope.term.url);
 			});
 		}
 	}
-});
+})
+
+.directive("contentMain", ["$routeParams", function($routeParams){
+	return{
+		restrict: "AEC",
+		templateUrl: "views/"+$routeParams.url+".html",
+		link: function(){
+
+		}
+	}
+}]);
+
+// .directive("contentMain", function($compile, $http, $templateCache, $parse){
+// 	return{
+// 		restrict: "AEC",
+// 		//scope: {url: "@url"},
+// 		// templateUrl: function(ele,attr){
+// 		// 	return "views/"+attr.type+".html"
+// 		// },
+// 		//controller: 'contentCtrl',
+// 		//templateUrl: "views/{{url}}.html",
+// 		//templateUrl: "views/ubuntu.html",
+// 		template: "<p>ubuntu</p>",
+// 		link: function(scope , element, attrs) {
+
+// 			scope.$watch(attrs.type, function (value) {
+// 				if (value) {
+// 					loadTemplate(value);
+// 				}
+// 			});
+
+// 			function loadTemplate(template) {
+// 				$http.get("views/"+template+".html", { cache: $templateCache })
+// 				.success(function(templateContent) {
+// 					element.replaceWith($compile(templateContent)(scope));                
+// 				});    
+// 			}
+// 		} 
+// 	}
+// });
